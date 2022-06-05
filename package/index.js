fa539f4198e78c8ae49dbf0fd7233d10eb4f1d73
Ulises Jeremias Cornejo Fandos <ulisescf.24@gmail.com>
2022-06-05 02:24:04 -0300

    Update index.js

--- package/index.js
+++ package/index.js
@@ -5,8 +5,8 @@ module.exports = function resolvePackage(setup, { appName, command }) {
     "docker:dev:build": `docker build -f docker/web/Dockerfile --target development -t ${appName} .`,
     "docker:dev:start": `docker run --rm -it --network host -v $PWD:/usr/src/app ${appName}`,
     "docker:prod": `${command} docker:prod:build && ${command} docker:prod:start`,
-    "docker:prod:build": `docker build --build-arg API_BASE_URL=$API_BASE_URL -f docker/web/Dockerfile --target production -t ${appName}:production .`,
-    "docker:prod:start": `docker run --rm -it --network host -e API_BASE_URL=$API_BASE_URL -v $PWD:/usr/src/app ${appName}:production`,
+    "docker:prod:build": `docker build -f docker/web/Dockerfile --target production -t ${appName}:production .`,
+    "docker:prod:start": `docker run --rm -it --network host -v $PWD:/usr/src/app ${appName}:production`,
   };
 
   return { ...setup, scripts };
